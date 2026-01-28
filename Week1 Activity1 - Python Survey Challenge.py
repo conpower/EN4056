bddd23f630d759731f6830dc1d52b2d864de4506
Conor Power <conpower@gmail.com>
2026-01-28 12:59:27 +0000

    Add files via upload

---
+++ Week1 Activity1 - Python Survey Challenge.py
@@ -0,0 +1,27 @@
+# Python Survey Challenge – Starter Template
+
+print("Learning Outcome Survey")
+print("------------------------")
+print("Learning Outcome: 1.16 compare two different user interfaces and identify different design decisions that shape the user experience\n")
+
+qualitative_responses = []
+
+number_of_students = int(input("How many students are completing the survey? "))
+
+for i in range(number_of_students):
+    print("\nStudent", i + 1)
+
+    # Quantitative question (1–5 scale)
+    
+
+    # Qualitative question
+    
+
+# Display results
+print("\n--- Survey Results ---")
+
+print("\nQuantitative Ratings:")
+
+
+print("\nQualitative Feedback:")
+
